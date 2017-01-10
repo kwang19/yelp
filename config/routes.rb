a9e9043af8a650af2e06d1bf37a0f72da5e40c27
Raghu Betina <raghu@firstdraft.com>
2017-01-10 02:44:33 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "reviews#index"
   # Routes for the Restaurant resource:
   # CREATE
   get "/restaurants/new", :controller => "restaurants", :action => "new"
