add21d70a35a4281846e19b9c2597918f109f7b0
Raghu Betina <raghu@firstdraft.com>
2017-01-10 18:31:10 +0000

    9th draft

--- db/migrate/20170110182954_create_reviewers.rb
+++ db/migrate/20170110182954_create_reviewers.rb
@@ -1,8 +1,8 @@
 class CreateReviewers < ActiveRecord::Migration
   def change
     create_table :reviewers do |t|
-      t.integer :user_id
       t.string :name
+      t.integer :user_id
       t.integer :reviews_count
       t.float :average_rating
 
